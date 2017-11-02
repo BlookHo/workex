0407f8f5580142a9e5789bd7d61bce816d74471c
AlekseyI <zoneiva@gmail.com>
2017-11-02 11:34:22 +0300

    model theme

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026064611) do
+ActiveRecord::Schema.define(version: 20171102073544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "themes", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "qty_items"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
